af22b896ff417fdd75a3c7f87ec58a5ceba95ec5
stakanich <49338156+stakanich@users.noreply.github.com>
2019-10-03 12:42:36 +0300

    Update mandelbrod.js

--- mandelbrod.js
+++ mandelbrod.js
@@ -3,8 +3,8 @@ function createImage()
     //console.log("Drawing");
     var c = document.getElementById("someCanvas");
     var ctx = c.getContext("2d");
-    var wgt = 200
-    var hgt = 200
+    var wdt = 400
+    var hgt = 400
 
     function drawPix(x, y, r, g, b)
     {
@@ -27,9 +27,9 @@ function createImage()
     {
         for (var ky = 0.0; ky < hgt; ky++)
         {
-            for (var kx = 0.0; kx < wgt; kx++)
+            for (var kx = 0.0; kx < wdt; kx++)
             {
-                var ca = xa + (((xb-xa)*kx) / wgt);
+                var ca = xa + (((xb-xa)*kx) / wdt);
                 var cb = ya + (((yb-ya)*ky) / hgt);
 
                 var za = 0;
@@ -57,6 +57,33 @@ function createImage()
             }
         }
     }
+    
+    function getMousePos(canv, evt)
+    {
+        var rect = canv.getBoundingClientRect();
+        return
+        {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+        };
+    }
+    
+    c.addEventListener('mousedown', function(evt){
+        var coords = getMousePos(c, evt);
+        nxtxa = curxa + (((curxb - curxa) * coords.x) / wdt;
+        nxtya = curyx + (((curyb - curya) * coords.y) / hgt;
+    });
+    
+    c.addEventListener('mouseup', function(evt){
+        var coords = getMousePos(c, evt);
+        nxtxb = curxa + (((curxb - curxa) * coords.x) / wdt;
+        nxtyb = nxtya + nxtxb - nxtxa;
+        curxa = nxtxa;
+        curxb = nxtxb;
+        curya = nxtya;
+        curyb = nxtyb;
+        drawSet(nxtxa, nxtxb, nxtya, nxtyb);
+    }):
     
     drawSet(nxtxa, nxtxb, nxtya, nxtyb);
     
